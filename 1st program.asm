670026844c0c533d86cb2b69307fb86c0a85578e
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-02-14 09:42:58 +0500

    First Program and Lab

---
+++ 1st program.asm
@@ -0,0 +1,15 @@
+.model small;model segment there are 6 types of models
+;--tiny
+;--small
+;--medium
+;--compact
+;--large
+;--huge
+
+.stack 100h; stack segment of 100hex
+.data; data segment to define all the variables
+.code; code segment is all the code part
+main proc; main procedure LIKE main func in cpp
+    
+    main endp; to end main procedure
+end main; like return 0 in cpp
