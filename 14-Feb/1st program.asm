d18f46abb1664fcc1af3530acb93f89b2f347816
Muhammad Raffey <70153209@student.uol.edu.pk>
2025-02-21 04:44:31 +0000

    Clarify main procedure description in 1st program.asm

--- 14-Feb/1st program.asm
+++ 14-Feb/1st program.asm
@@ -9,7 +9,7 @@
 .stack 100h; stack segment of 100hex
 .data; data segment to define all the variables
 .code; code segment is all the code part
-main proc; main procedure LIKE main func in cpp
+main proc; main procedure LIKE main func in cpp  Entery point of code segment 
     
     main endp; to end main procedure
 end main; like return 0 in cpp
